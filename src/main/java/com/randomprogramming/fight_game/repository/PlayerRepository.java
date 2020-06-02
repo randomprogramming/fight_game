2af5091bed577da21a681607ce9e8ac9ff7ab8f7
randomprogramming <isnipedodac@gmail.com>
2020-06-02 19:10:53 +0200

    added correct repository annotations

--- src/main/java/com/randomprogramming/fight_game/repository/PlayerRepository.java
+++ src/main/java/com/randomprogramming/fight_game/repository/PlayerRepository.java
@@ -2,7 +2,9 @@ package com.randomprogramming.fight_game.repository;
 
 import com.randomprogramming.fight_game.entity.Player;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepository extends CrudRepository<Player, Long> {
 
 }
